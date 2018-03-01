6469da28958e79dd3bd32ac2515b9cc10922cbfd
Divya Mohan <21dmohan@berkeley.edu>
2018-02-28 20:07:50 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main {
+    private static final String FILE_PATH = "C:\\PATH\\NJIT\\src\\supermarket_test.csv";
+
+    public static void main(String args[]) {
+        try {
+            File file = new File(FILE_PATH);
+            Scanner scanner = new Scanner(file);
+
+            Map<String, Double> customers = new HashMap<String, Double>();
+            String[] transactions;
+
+            transactions = scanner.nextLine().split(","); //disregard first line
+            while(scanner.hasNextLine()) {
+                transactions = scanner.nextLine().split(",");
+                if (customers.get(transactions[4]) == null) {
+                    customers.put(transactions[4],
+                            Double.parseDouble(transactions[5]) *
+                            Double.parseDouble(transactions[6]));
+                } else {
+                    customers.put(transactions[4],
+                            customers.get(transactions[4]) +
+                                    Double.parseDouble(transactions[5]) *
+                                    Double.parseDouble(transactions[6]));
+                }
+            }
+
+            for (String c : customers.keySet()) {
+                System.out.println(c + " " + customers.get(c));
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+    }
+}
